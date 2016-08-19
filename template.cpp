9100a8f2e641be7fb06abab6d1c920118d3a0725
soytw <chengkongtw@gmail.com>
2016-08-19 14:32:33 +0800

    default code template

---
+++ template.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+
+#define PB push_back
+#define PPB pop_back
+#define MS(s,v) memset(s,v,sizeof(s))
+#define PI pair<int,int>
+#define PD pair<double,double>
+#define PL pair<long long,long long>
+#define FF first
+#define SS second
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+struct E {
+    int t,w;
+    E(int _t,int _w) {
+        t=_t, w=_w;
+    }
+    bool operator<(const E& e) const {
+        return w<e.w;
+    }
+};
+int N,M;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+
+    return 0;
+}
