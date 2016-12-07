b577b63e0dc577693b7609e778d6449fc85419a8
yo <yo>
2016-12-07 09:08:33 +0800

    knapsack & disjoint set

--- cf742d.cpp
+++ cf742d.cpp
@@ -1,3 +1,5 @@
+// cf742d
+// knapsack & disjoint set
 #include <bits/stdc++.h>
 #define int long long
 #define MS(s,v) memset(s,v,sizeof(s))
@@ -16,11 +18,11 @@ void debug(int n,int m) {
             cout<<dp[i][j]<<' ';
         cout<<endl;
     }
+    cout<<endl;
 }
 main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    freopen("in.txt","r",stdin);
     int n,m,W,a,b;
     cin>>n>>m>>W;
     FOR(i,1,n) cin>>w[i];
@@ -30,10 +32,8 @@ main() {
         cin>>a>>b;
         pa[find(a)]=find(b);
     }
-    FOR(i,1,n) {
-        grp[pa[i]].push_back(i);
-        // g++;
-    }
+    FOR(i,1,n)
+        grp[find(i)].push_back(i);
     MS(dp,0);
     int i,g=0;
     while (g<=n && grp[g].empty()) g++;
@@ -41,18 +41,15 @@ main() {
         FOR(j,0,W) {
             int sw=0,sv=0;
             for (int k:grp[g]) {
-                cout<<"i:"<<i<<" g:"<<g<<" j:"<<j<<" K:"<<k<<endl;
-                if (j<w[k]) dp[i][j]=dp[i-1][j];
-                else dp[i][j]=max(dp[i-1][j]+v[k],dp[i-1][j-w[k]]+v[k]);
+                if (j<w[k]) dp[i][j]=max(dp[i][j],dp[i-1][j]);
+                else dp[i][j]=max({dp[i][j],dp[i-1][j],dp[i-1][j-w[k]]+v[k]});
                 sw+=w[k], sv+=v[k];
             }
-            if (j>=sw)
-                dp[i][j]=max(dp[i-1][j],dp[i-1][j-sw]+sv);
+            if (j>=sw) dp[i][j]=max({dp[i][j],dp[i-1][j],dp[i-1][j-sw]+sv});
         }
         g++;
         while (g<=n && grp[g].empty()) g++;
     }
-    debug(i,W);
     cout<<*max_element(&dp[i-1][0],&dp[i-1][W]+1)<<'\n';
     return 0;
 }
