7f2a45a72c96372dd9fc4f989c54e91fcc42e80f
soytw <chengkongtw@gmail.com>
2017-02-06 01:47:44 +0800

    UNDONE!! Fast Matrix Exp

--- _uva10870.cpp
+++ _uva10870.cpp
@@ -1,3 +1,5 @@
+// uva10870
+// fast matrix exponentation
 #include <bits/stdc++.h>
 #define int long long
 #define MS(s,v) memset(s,v,sizeof(s))
@@ -7,56 +9,56 @@ using namespace std;
 const long long INF = 0x3f3f3f3f3f3f3f3fLL;
 
 struct Mat {
-	int a[20][20], r, c, m;
-	Mat() {
-		FOR(i,0,r) FOR(j,0,c) a[i][j]=0;
-	}
-	Mat operator*(const Mat& x) {
-		Mat y;
-		FOR(i,0,r) FOR(j,0,x.c) FOR(k,0,c)
-			y.a[i][j] += (a[i][k] + x.a[k][j]) % m;
-		return y;
-	}
-	Mat operator%(int m) {
-		FOR(i,0,r) FOR(j,0,c) a[i][j]%=c;
-	}
-	Mat operator^(int e) {
-		Mat y;
-		FOR(i,0,max(r,c)) a[i][i]=1;
-		while (e) {
-			if (e&1) y = y*(*this)%m;
-			*this = (*this)*(*this)%m;
-			e/=2;
-		}
-		return y;
-	}
+    int a[100][100], r, c, m;
+    Mat() {
+        MS(a,0);
+        r = c = 0;
+        m = 1;
+    }
+    Mat& operator=(const Mat& x) {
+        memcpy(a, x.a, sizeof a);
+        r=x.r, c=x.c, m=x.m;
+        return *this;
+    }
 } A, B;
+Mat unit_mat(int d, int m) {
+    Mat x;
+    x.r = x.c = d;
+    x.m = m;
+    FOR(i,0,d) x.a[i][i]=1;
+    return x;
+}
+ostream& operator<<(ostream& os, const Mat& x) {
+    FOR(i,0,x.r) {
+        FOR(j,0,x.c) os<<x.a[i][j]<<' ';
+        os<<endl;
+    }
+    return os;
+}
+Mat operator*(const Mat& x, const Mat& y) {
+    Mat z;
+    if (x.c != y.r) { cout<<"ERR\n"; return z; }
+    z.m=x.m, z.r=x.r, z.c=y.c;
+    // cout<<x.r<<x.c<<y.r<<y.c<<endl;
+    FOR(i,0,x.r) FOR(j,0,y.c) FOR(k,0,x.c)
+        z.a[i][j] += x.a[i][k] * y.a[k][j];// % x.m;
+    return z;
+}
+Mat operator%(Mat x, int m) {
+    FOR(i,0,x.r) FOR(j,0,x.c) x.a[i][j]%=m;
+    return x;
+}
+Mat operator^(Mat x, int e) {
+    Mat y = unit_mat(x.r, x.m);
+    while (e) {
+        // cout<<'e'<<e<<endl<<x<<y<<endl;
+        if (e & 1) y = y * x % x.m;
+        x = x * x % x.m;
+        e /= 2;
+    }
+    return y;
+}
 
-void print(int d) {
-	FOR(i,1,d+1) {
-		FOR(j,1,d+1)
-			cout<<A[i][j]<<' ';
-		cout<<endl;
-	}
-	FOR(i,1,d+1) {
-		FOR(j,1,d+1)
-			cout<<B[i][j]<<' ';
-		cout<<endl;
-	}
-	cout<<endl;
-}
-Mat pow(Mat a, int e, int r, int m) { // r==c
-	Mat b;
-	FOR(i,0,r) FOR(j,0,r)
-		if (i==j) b[i][j]=1;
-		else b[i][j]=0;
-	while (e) {
-		if (e&1) b=mul(b,a,r,r,r,m);
-		a = mul(a,a,r,r,r,m);
-		e /= 2;
-	}
-	return b;
-}
 main() {
 #ifdef soytw
     freopen("in.txt","r",stdin);
@@ -66,10 +68,15 @@ main() {
 #endif
     int n,d,m;
     while (cin>>d>>n>>m && d && n && m) {
-        FOR(i,1,d+1) cin>>A[1][i];
-        FOR(i,1,d+1) A[i+1][i]=1;
-        _FOR(i,d,0,-1) cin>>B[i][1];
-        print(d);
+        A = B = Mat();
+        A.r = A.c = B.r = d;
+        B.c = 1;
+        A.m = B.m = m;
+        for (int i=d-1; i>=0; i--) cin>>A.a[0][i];
+        FOR(i,0,d) A.a[i+1][i]=1;
+        for (int i=d-1; i>=0; i--) cin>>B.a[i][0];
+        A=A%m, B=B%m;
+        cout<<((A^(n-1))*B).a[d-1][0]%m<<endl;
     }
 #ifdef soytw
     freopen("con","r",stdin);
