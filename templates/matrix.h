0c7207224107f45c65c4c2dfc404becf568debdf
soytw <soytwww@gmail.com>
2017-03-03 19:56:50 +0800

    update matrix.h

--- templates/matrix.h
+++ templates/matrix.h
@@ -1,3 +1,4 @@
+// TODO: inverse matrix and augmented matrix
 #ifndef _MATRIX_H
 #define _MATRIX_H
 #define FOR(i,start,end) for (size_t i=start; i<end; ++i)
@@ -29,42 +30,11 @@ using std::is_integral;
 
 template<typename T>
 class Matrix {
-protected:
-    vector<vector<T>> _arr;
-    size_t _rows, _cols;
-
 public:
-    template<typename U>
-    friend Matrix<U> operator+(Matrix<U> x, const Matrix<U>& y);
-
-    template<typename U>
-    friend Matrix<U> operator-(Matrix<U> x, const Matrix<U>& y);
-
-    template<typename U>
-    friend Matrix<U> operator*(Matrix<U> x, U y);
-
-    template<typename U>
-    friend Matrix<U> operator/(Matrix<U> x, U y);
-
-    template<typename U>
-    friend ostream& operator<<(ostream& os, const Matrix<U>& x);
-
-    template<typename U>
-    friend Matrix<U> operator*(const Matrix<U>& x, const Matrix<U>& y);
-
-    template<typename U, typename Int>
-    friend Matrix<U> operator^(const Matrix<U>& x, Int y);
-
-    template<typename U, typename Int>
-    friend Matrix<U> mul(const Matrix<U>& x, const Matrix<U>& y, Int mod);
-
-    template<typename U, typename Int>
-    friend Matrix<U> pow(Matrix<U> x, Int y, Int mod);
-
     Matrix(size_t rows=0, size_t cols=0) {
-        this->_arr.resize(rows, vector<T>(cols));
-        this->_rows = rows;
-        this->_cols = cols;
+        _arr.resize(rows, vector<T>(cols));
+        _rows = rows;
+        _cols = cols;
     }
     vector<T>& operator[](size_t index) {
         return _arr[index];
@@ -72,149 +42,151 @@ public:
     Matrix& operator+=(const Matrix& x) {
         if (_rows != x._rows || _cols != x._cols)
             throw invalid_argument("matrix size not match");
-        FOR(i, 0, this->_rows)
-            FOR(j, 0, this->_cols)
-                this->_arr[i][j] += x._arr[i][j];
+        FOR(i, 0, _rows)
+            FOR(j, 0, _cols)
+                _arr[i][j] += x._arr[i][j];
         return *this;
     }
     Matrix& operator-=(const Matrix& x) {
-        if (_rows != x._rows || _cols != x._cols)
-            throw invalid_argument("matrix size not match");
-        FOR(i, 0, this->_rows)
-            FOR(j, 0, this->_cols)
-                this->_arr[i][j] -= x._arr[i][j];
-        return *this;
+        return *this += -x;
     }
     Matrix& operator*=(T t) {
-        FOR(i, 0, this->_rows)
-            FOR(j, 0, this->_cols)
-                this->_arr[i][j] *= t;
+        FOR(i, 0, _rows)
+            FOR(j, 0, _cols)
+                _arr[i][j] *= t;
         return *this;
     }
     Matrix& operator/=(T t) {
-        FOR(i, 0, this->_rows)
-            FOR(j, 0, this->_cols)
-                this->_arr[i][j] /= t;
+        FOR(i, 0, _rows)
+            FOR(j, 0, _cols)
+                _arr[i][j] /= t;
         return *this;
     }
     Matrix& operator%=(T t) {
-        FOR(i, 0, this->_rows)
-            FOR(j, 0, this->_cols)
-                this->_arr[i][j] %= t;
+        FOR(i, 0, _rows)
+            FOR(j, 0, _cols)
+                _arr[i][j] %= t;
         return *this;
+    }
+    int gaussian_elimination() {
+        // TODO: augumented matrix
+        bool sign = 0; // whether determinant should change sign
+        FOR(i, 0, _rows) {
+            for (size_t j=i+1; j<_rows && _arr[i][i]==0; j++) {
+                swap(_arr[i], _arr[j]);
+                sign = !sign;
+            }
+            if (_arr[i][i] == 0)
+                // fail
+                return -1;
+            FOR(j, 0, _rows) {
+                if (j != i) {
+                    T t = _arr[j][i] / _arr[i][i];
+                    FOR(k, 0, _cols) {
+                        _arr[j][k] -= _arr[i][k] * t;
+                    }
+                }
+            }
+        }
+        return sign;
+    }
+    Matrix& transpose() {
+        FOR(i, 0, _rows)
+            FOR(j, i, _cols)
+                swap(_arr[i][j], _arr[j][i]);
+        return *this;
+    }
+    T determinant() const {
+        if (_rows != _cols)
+            throw invalid_argument("matrix should be square");
+        Matrix x = *this;
+        int sign = x.gaussian_elimination();
+        T res = 1;
+        FOR(i, 0, x._rows) res *= x._arr[i][i];
+        if (sign == 1) return -res;
+        else if (sign == 0) return res;
+        else return 0;
+    }
+    friend Matrix operator-(Matrix x) { return x *= -1; }
+    friend Matrix operator+(Matrix x, const Matrix& y) { return x += y; }
+    friend Matrix operator-(Matrix x, const Matrix& y) { return x -= y; }
+    friend Matrix operator*(Matrix x, T y) { return x *= y; }
+    friend Matrix operator/(Matrix x, T y) { return x /= y; }
+    friend Matrix operator%(Matrix x, T y) { return x %= y; }
+    friend ostream& operator<<(ostream& os, const Matrix& x) {
+        FOR(i, 0, x._rows) {
+            FOR(j, 0, x._cols) {
+                os << x._arr[i][j];
+                if (j == x._cols-1)
+                    os << '\n';
+                else
+                    os << ',' << ' ';
+            }
+        }
+        return os;
+    }
+    friend Matrix operator*(const Matrix& x, const Matrix& y) {
+        return mul(x, y);
+    }
+    template<typename Int>
+    friend Matrix operator^(const Matrix& x, Int y) {
+        return pow(x, y);
+    }
+    template<typename Int=int>
+    static Matrix mul(const Matrix& x, const Matrix& y, Int mod=Int()) {
+        // modulo acts only when T == Int
+        static_assert(is_integral<Int>::value, "modulo not integral");
+        if (x._cols != y._rows)
+            throw invalid_argument(
+                "matrix size not suitable for multiplying\n"
+                "it should be: (m*n) and (n*p) respectively\n"
+                "try changing their order");
+        Matrix z(x._rows, y._cols);
+        FOR(i, 0, x._rows)
+            FOR(j, 0, y._cols)
+                FOR(k, 0, x._cols) {
+                    z._arr[i][j] += x._arr[i][k] * y._arr[k][j];
+                    if (mod != Int())
+                        // the same as: z._arr[i][j] %= mod
+                        // handle floating types, which cannot do modulo
+                        _mod_assign(z._arr[i][j], mod);
+                }
+        return z;
+    }
+    template<typename Int>
+    static Matrix pow(Matrix x, Int y, Int mod=Int()) {
+        // quick exponentation
+        static_assert(is_integral<Int>::value, "modulo or exponent not integral");
+        if (x._rows != x._cols)
+            throw invalid_argument("matrix should be square");
+        Matrix z = Matrix::identity_matrix(x._rows);
+        while (y > 0) {
+            if (y % 2 == 1)
+                z = mul(x, z, mod);
+            x = mul(x, x, mod);
+            y /= 2;
+        }
+        return z;
     }
     static Matrix identity_matrix(size_t size) {
         Matrix x(size, size);
         FOR(i, 0, size) x._arr[i][i]=1;
         return x;
     }
-    bool gaussian_elimination() {
-        FOR(i, 0, this->_rows) {
-            for (size_t j=i+1; j<this->_rows && this->_arr[i][i]==0; j++)
-                swap(this->_arr[i], this->_arr[j]);
-            if (this->_arr[i][i] == 0) return false;
-            FOR(j, 0, this->_rows) {
-                if (j != i) {
-                    T t = this->_arr[j][i] / this->_arr[i][i];
-                    FOR(k, 0, this->_cols) {
-                        this->_arr[j][k] -= this->_arr[i][k] * t;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    Matrix& transpose() {
-        FOR(i, 0, this->_rows)
-            FOR(j, i, this->_cols)
-                swap(this->_arr[i][j], this->_arr[j][i]);
-        return *this;
-    }
+
+protected:
+    vector<vector<T>> _arr;
+    size_t _rows, _cols;
+
+private:
+    // prevent something like (double) %= (int)
+    template<typename T1, typename T2>
+    static T1& _mod_assign(T1& x, T2 mod) { return x; }
+
+    static int& _mod_assign(int& x, int mod) { return x %= mod; }
+    static long long& _mod_assign(long long& x, long long mod) { return x %= mod; }
 };
 
-template<typename T>
-Matrix<T> operator+(Matrix<T> x, const Matrix<T>& y) { return x += y; }
-
-template<typename T>
-Matrix<T> operator-(Matrix<T> x, const Matrix<T>& y) { return x -= y; }
-
-template<typename T>
-Matrix<T> operator*(Matrix<T> x, T y) { return x *= y; }
-
-template<typename T>
-Matrix<T> operator/(Matrix<T> x, T y) { return x /= y; }
-
-template<typename T>
-Matrix<T> operator%(Matrix<T> x, T y) { return x %= y; }
-
-template<typename T>
-ostream& operator<<(ostream& os, const Matrix<T>& x) {
-    FOR(i, 0, x._rows) {
-        FOR(j, 0, x._cols) {
-            os << x._arr[i][j];
-            if (j == x._cols-1)
-                os << '\n';
-            else
-                os << ' ';
-        }
-    }
-    return os;
-}
-
-template<typename T>
-Matrix<T> operator*(const Matrix<T>& x, const Matrix<T>& y) {
-    return mul(x, y);
-}
-
-template<typename T, typename Int>
-Matrix<T> operator^(const Matrix<T>& x, Int y) {
-    return pow(x, y);
-}
-
-template<typename T, typename U>
-T& _mod_assign(T& x, U mod) { return x; }
-
-template<>
-int& _mod_assign(int& x, int mod) { return x %= mod; }
-
-template<>
-long long& _mod_assign(long long& x, long long mod) { return x %= mod; }
-
-template<typename T, typename Int=int>
-Matrix<T> mul(const Matrix<T>& x, const Matrix<T>& y, Int mod=Int()) {
-    // modulo acts only when T == Int
-    static_assert(is_integral<Int>::value, "modulo not integral");
-    if (x._cols != y._rows)
-        throw invalid_argument(
-            "matrix size not suitable for multiplying\n"
-            "it should be: (m*n) and (n*p) respectively\n"
-            "try changing their order");
-    Matrix<T> z(x._rows, y._cols);
-    FOR(i, 0, x._rows)
-        FOR(j, 0, y._cols)
-            FOR(k, 0, x._cols) {
-                z._arr[i][j] += x._arr[i][k] * y._arr[k][j];
-                if (mod != Int())
-                    _mod_assign(z._arr[i][j], mod);
-            }
-    return z;
-}
-
-// quick exponentation
-template<typename T, typename Int>
-Matrix<T> pow(Matrix<T> x, Int y, Int mod=Int()) {
-    static_assert(is_integral<Int>::value, "modulo or exponent not integral");
-    if (x._rows != x._cols)
-        throw invalid_argument("matrix should be square");
-    Matrix<T> z = Matrix<T>::identity_matrix(x._rows);
-    while (y > 0) {
-        if (y % 2 == 1)
-            z = mul(x, z, mod);
-        x = mul(x, x, mod);
-        y /= 2;
-    }
-    return z;
-}
 
 #undef FOR
 #endif
