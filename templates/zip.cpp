18154a404819704f712397b713212426b2130ea1
soytw <soytwww@gmail.com>
2017-04-03 21:18:28 +0800

    .

--- templates/zip.cpp
+++ templates/zip.cpp
@@ -1,3 +1,4 @@
+// PKZIP version 2.0
 #include <bits/stdc++.h>
 #define FOR(i,end) for (int i=0; i<end; ++i)
 #define FOR1(i,end) for (int i=1; i<=end; ++i)
@@ -20,7 +21,7 @@ struct Block {
 
 class MyZipEncoder {
 public:
-    static constexpr size_t MAX_BUFFER_SIZE = 65535;
+    static constexpr size_t MAX_BUFFER_SIZE = 1048576; // 1M
     static constexpr size_t MAX_FIELD_LEN = 65535;
     // 32KiB for search block and for string block, 64KiB in total
 
@@ -49,55 +50,76 @@ public:
     //             search = cur - block_size;
     //     }
     // }
-    int decode(char* src) {}
+    int decode(istream& src, streampos begin, size_t size) {
+        DEBUGMSG("[decode] begin: %llu size: %llu\n\n", (size_t)begin, size);
+        src.seekg(size, src.cur);
+        return 0;
+    }
     int read_header(istream& input) {
         while (input.good()) {
+            cout<<"tellg:"<<input.tellg()<<endl;
+
             input.read((char*)buffer, 4);
             signature = *(uint32_t*)(buffer);
+            DEBUGMSG("sign: 0x%x\n\n", signature);
             if (signature == 0x04034b50) {
-                // -- LOCAL FILE HEADER --
+                // local file header
                 input.read((char*)buffer+4, 26);
-                extract_version = *(uint16_t*)(buffer+4);
-                purpose = *(uint16_t*)(buffer+6);
-                method = *(uint16_t*)(buffer+8);
-                modtime = *(uint16_t*)(buffer+10);
-                moddate = *(uint16_t*)(buffer+12);
-                crc32 = *(uint32_t*)(buffer+14);
-                compressed_size = *(uint32_t*)(buffer+18);
-                uncompressed_size = *(uint32_t*)(buffer+22);
-                filename_len = *(uint16_t*)(buffer+26);
-                extrafield_len = *(uint16_t*)(buffer+28);
+                uint16_t extract_version = *(uint16_t*)(buffer+4);
+                uint16_t purpose = *(uint16_t*)(buffer+6);
+                uint16_t method = *(uint16_t*)(buffer+8);
+                uint16_t modtime = *(uint16_t*)(buffer+10);
+                uint16_t moddate = *(uint16_t*)(buffer+12);
+                uint32_t crc32 = *(uint32_t*)(buffer+14);
+                uint32_t compressed_size = *(uint32_t*)(buffer+18);
+                uint32_t uncompressed_size = *(uint32_t*)(buffer+22);
+                uint16_t filename_len = *(uint16_t*)(buffer+26);
+                uint16_t extrafield_len = *(uint16_t*)(buffer+28);
 
                 input.read(filename, filename_len);
                 filename[filename_len] = '\0';
                 input.read(extrafield, extrafield_len);
                 extrafield[extrafield_len] = '\0';
 
+                if (purpose & 1) {
+                    // [TODO] encrypted
+                }
+                if (purpose & 2) {
+                    // [TODO] compression option
+                }
+                if (purpose & 4) {
+                    // [TODO] compression option
+                }
+                if (purpose & 8) {
+                    // data descriptor
+                    input.read(buffer, 12);
+                    crc32 = *(uint32_t*)(buffer);
+                    compressed_size = *(uint32_t*)(buffer+4);
+                    uncompressed_size = *(uint32_t*)(buffer+8);
+                }
+
+                struct tm moddt = dosdatetime(moddate, modtime);
+
+
                 DEBUGMSG("file: %s\n", filename);
-                DEBUGMSG("size: %d\n", uncompressed_size);
-                DEBUGMSG("compressed size: %d\n", compressed_size);
+                DEBUGMSG("extract_version: %hu\n", extract_version);
+                DEBUGMSG("purpose: %hx\n", purpose);
+                DEBUGMSG("method: %hx\n", method);
+                DEBUGMSG("modtime: %u:%u:%u\n", moddt.tm_hour, moddt.tm_min, moddt.tm_sec);
+                DEBUGMSG("moddate: %u.%u.%u\n", 1900+moddt.tm_year, 1+moddt.tm_mon, moddt.tm_mday);
+                DEBUGMSG("crc32: %x\n", crc32);
+                DEBUGMSG("compressed size: %u\n", compressed_size);
+                DEBUGMSG("uncompressed size: %u\n", uncompressed_size);
+                DEBUGMSG("filename_len: %hu\n", filename_len);
+                DEBUGMSG("extrafield_len: %hu\n", extrafield_len);
                 DEBUGMSG("\n");
-                // -- END LOCAL FILE HEADER --
 
-                // [TODO] ENCRYPTION FILE HEADER
+                // file data
+                decode(input, input.tellg(), compressed_size);
 
-                // -- FILE DATA --
-                input.read((char*)buffer, compressed_size);
-                decode(buffer);
-                // -- END FILE DATA --
-
-                // [TODO] DATA DESCRIPTOR
-            } else if (signature == 0x08064b50) {
-                // ARCHIVE EXTRA DATA RECORD
-                input.read((char*)(buffer+4), 4);
-                extrafield_len = *(uint32_t*)(buffer+4);
-                input.read(extrafield, extrafield_len);
-                extrafield[extrafield_len] = '\0';
-                DEBUGMSG("archive extrafield: %s\n", extrafield);
-                // END ARCHIVE EXTRA DATA RECORD
             } else if (signature == 0x02014b50) {
-                // CENTRAL DIRECTORY STRUCTURE
-                input.read(buffer+4, 46);
+                // central directory header
+                input.read(buffer+4, 42);
                 made_version = *(uint16_t*)(buffer+4);
                 extract_version = *(uint16_t*)(buffer+6);
                 purpose = *(uint16_t*)(buffer+8);
@@ -122,38 +144,68 @@ public:
                 input.read(comment, comment_len);
                 comment[comment_len] = '\0';
 
-                DEBUGMSG("[DIR] filename: %s\n", filename);
-                DEBUGMSG("extrafield: %s\n", extrafield);
-                DEBUGMSG("comment: %s\n", comment);
+                DEBUGMSG("file: %s\n", filename);
+                DEBUGMSG("made_version: %hu\n", made_version);
+                DEBUGMSG("extract_version: %hu\n", extract_version);
+                DEBUGMSG("purpose: %hx\n", purpose);
+                DEBUGMSG("method: %hx\n", method);
+                DEBUGMSG("modtime: %hx\n", modtime);
+                DEBUGMSG("moddate: %hx\n", moddate);
+                DEBUGMSG("crc32: %x\n", crc32);
+                DEBUGMSG("compressed size: %u\n", compressed_size);
+                DEBUGMSG("uncompressed size: %u\n", uncompressed_size);
+                DEBUGMSG("filename_len: %hu\n", filename_len);
+                DEBUGMSG("extrafield_len: %hu\n", extrafield_len);
+                DEBUGMSG("comment_len: %hu\n", comment_len);
+                DEBUGMSG("disknum: %hx\n", disknum);
+                DEBUGMSG("inter_attr: %hx\n", inter_attr);
+                DEBUGMSG("exter_attr: %x\n", exter_attr);
+                DEBUGMSG("offset: %u\n", offset);
                 DEBUGMSG("\n");
-                // END CENTRAL DIRECTORY STRUCTURE
             } else if (signature == 0x05054b50) {
-                // DIGITAL SIGNATURE
+                // digital signature
                 input.read(buffer+4, 2);
                 uint16_t datasize = *(uint16_t*)(buffer+4);
                 input.read(buffer, datasize);
                 DEBUGMSG("digital sign: %.*s\n", datasize, buffer);
-                // END DIGITAL SIGNATURE
+            } else if (signature == 0x06054b50) {
+                // end of central directory record
+                // input.read(buffer+4, );
+                // disknum = *(uint16_t*)(buffer+4);
+                // disknum_with_start = *(uint16_t*)(buffer+6);
+                // entry_count = *(uint16_t*)(buffer+8);
+            } else {
+                DEBUGMSG("unknown signature: %x\n", signature);
             }
         }
         return 0;
     }
 private:
-    char buffer[MAX_BUFFER_SIZE],
-         filename[MAX_FIELD_LEN],
-         extrafield[MAX_FIELD_LEN],
-         comment[MAX_FIELD_LEN];
+    struct FileHeader {
+        char filename[MAX_FIELD_LEN],
+             extrafield[MAX_FIELD_LEN],
+             comment[MAX_FIELD_LEN];
+    };
+
+    char buffer[MAX_BUFFER_SIZE];
     // TODO: prevent buffer overflow
-    uint32_t signature, crc32, compressed_size, uncompressed_size, exter_attr, offset;
-    uint16_t extract_version, made_version,
-             purpose, method, modtime, moddate,
-             filename_len, extrafield_len, comment_len,
-             disknum, inter_attr;
+
+    struct tm dosdatetime(uint16_t date, uint16_t time) {
+        struct tm datetime;
+        datetime.tm_mday = date & 0x1f;
+        datetime.tm_mon = ((date & 0x1e0) >> 5) - 1;
+        datetime.tm_year = ((date & 0xfe00) >> 9) + 80;
+        datetime.tm_sec = time & 0x1f;
+        datetime.tm_min = (time & 0x7e0) >> 5;
+        datetime.tm_hour = (time & 0xf800) >> 11;
+        return datetime;
+    }
 };
 
 char str[10000];
 int main() {
-    fstream infile("abccc.zip", ios::in | ios::binary);
+    fstream infile("a.zip", ios::in | ios::binary);
+    // file size: 464389
     if (infile.is_open()) {
         MyZipEncoder z;
         int result = z.read_header(infile);
