3ef58840f255d7ef914bb749ada7969da00dca1e
soytw <soytwww@gmail.com>
2019-03-01 16:00:15 +0800

    update poj2104 seg_kth_num to serve duplicate items

--- online-judge/poj2104_seg_kth_num.cpp
+++ online-judge/poj2104_seg_kth_num.cpp
@@ -6,10 +6,17 @@
 #include <climits>
 #include <algorithm>
 #define FOR(i, a, n) for (int i=a; i<n; i++)
+#define PII pair<int, int>
 using namespace std;
 
 int TR[20][112345];
 int A[112345];
+
+PII operator+=(PII& a, const PII& b) {
+    a.first += b.first;
+    a.second += b.second;
+    return a;
+}
 
 void build(int no, int l, int r) {
     if (l == r) {
@@ -33,21 +40,23 @@ void build(int no, int l, int r) {
     // putchar('\n');
 }
 
-int query(int no, int l, int r, int ql, int qr, int k) {
-    // return the number of items that is less than k
+PII query(int no, int l, int r, int ql, int qr, int k) {
+    // return (a,b) where:
+    // a = the number of items that is less than k
+    // b = the number of items that is less than or equal to k
 
     int m = (l + r) >> 1;
-    int ret = 0;
+    PII ret = make_pair(0, 0);
     if (ql <= l && r <= qr) {
-        ret = lower_bound(&TR[no][l], &TR[no][r+1], k) - &TR[no][l];
+        ret.first = lower_bound(&TR[no][l], &TR[no][r+1], k) - &TR[no][l];
+        ret.second = upper_bound(&TR[no][l], &TR[no][r+1], k) - &TR[no][l];
     }
     else {
-        if (ql <= m) {
+        if (ql <= m)
             ret += query(no+1, l, m, ql, qr, k);
-        }
-        if (qr > m) {
+
+        if (qr > m)
             ret += query(no+1, m+1, r, ql, qr, k);
-        }
     }
 
     // printf("no=%d l=%d r=%d ql=%d qr=%d k=%d ret=%d\nTR:", no, l, r, ql, qr, k, ret);
@@ -73,17 +82,18 @@ int main() {
                 int m = (l + r) >> 1;
                 if (m + 1 <= r)
                     m++; // +1 to prevent loop during binary search
-                int res = query(0, 1, n, ql, qr, m);
-                // printf("l=%d r=%d m=%d res=%d\n", l, r, m, res);
-                if (res == k-1)
+                PII res = query(0, 1, n, ql, qr, m);
+                // printf("l=%lld r=%lld m=%d res=(%d,%d)\n", l, r, m, res.first, res.second);
+                if (res.first < k && k <= res.second)
                     l = m;
-                else if (res < k-1)
+                else if (res.second < k)
                     l = m + 1;
-                else if (res > k-1)
+                else if (res.first > k-1)
                     r = m - 1;
             }
             assert(l == r);
-            assert(query(0, 1, n, ql, qr, l) == k-1);
+            PII res = query(0, 1, n, ql, qr, l);
+            assert(res.first < k && k <= res.second);
             printf("%lld\n", l);
         }
     }
